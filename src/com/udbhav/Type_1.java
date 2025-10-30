ad5b6a815f2efa5943034e450b6229819e897324
Udbhav <udbhavkumar54@gmail.com>
2025-10-30 07:44:11 +0530

    Initial Commit

---
+++ src/com/udbhav/Type_1.java
@@ -0,0 +1,10 @@
+package com.udbhav;
+
+public class Type_1 {
+    public static void main(String[] args) {
+        greet(); // calling a greet method
+    }
+    static void greet(){
+        System.out.println("Good Morning!!");
+    }
+}
