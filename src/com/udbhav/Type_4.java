d6bd7adf7796314db54b6d0cca9f8bc23f2dc33a
Udbhav <udbhavkumar54@gmail.com>
2025-10-30 08:09:54 +0530

    Type_4 Method

---
+++ src/com/udbhav/Type_4.java
@@ -0,0 +1,14 @@
+package com.udbhav;
+
+public class Type_4 {
+    public static void main(String[] args) {
+        int res = add(10,60);
+        System.out.println(res);
+    }
+    static int add(int a,int b){
+        int c = a + b;
+        return c;
+    }
+}
+
+// Method have parameter or input, have return type
