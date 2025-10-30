bc78954392c54d26bb5640b2bfc482a7bcffa4ce
Udbhav <udbhavkumar54@gmail.com>
2025-10-30 08:05:10 +0530

    Type_3 Method

---
+++ src/com/udbhav/Type_3.java
@@ -0,0 +1,16 @@
+package com.udbhav;
+
+public class Type_3 {
+    public static void main(String[] args) {
+        int res = add();
+        System.out.println(res);
+    }
+    static int add(){
+        int a = 10;
+        int b = 30;
+        int c = a + b;
+        return c;
+    }
+}
+
+// In this type method have no parameter but have return type
